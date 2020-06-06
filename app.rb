58be64b510bb4e427f438fa0e7dae57f7e1123a7
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-06-06 15:41:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,4 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  get '/' do
+    erb :index
+  end
+  
+  configure do
+    enable :sessions
+    session_secret, "secret"
+  end
+  
 end
